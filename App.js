ddb8bcbc87c1459c969bd1877525653594c10be5
Toan200 <toan434037@gmail.com>
2023-12-20 21:20:05 +0700

    lab45

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+import Login from './screens/Login';
+import Route from './screens/Route';
+
+const App = () => {
+  return (
+      <Route />
+  );
+};
+
+export default App
